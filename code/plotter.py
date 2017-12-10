ecd8fad3c45ff9a84760c854cafa5f11e631f531
Nick Schorr <naschorr@gmail.com>
2017-12-10 01:44:30 -0500

    Added triangle that points in the direction the plane is travelling
    - Triangle is placed at the spot where PlanePal was invoked
    - Triangle can be customized via config.json

--- code/plotter.py
+++ code/plotter.py
@@ -115,6 +115,8 @@ class Plotter:
     SHORT_PARACHUTE_PATH_COLOR_KEY = "short_parachute_path_color"
     LONG_PARACHUTE_PATH_WIDTH_KM_KEY = "long_parachute_path_width_km"
     LONG_PARACHUTE_PATH_COLOR_KEY = "long_parachute_path_color"
+    TRIANGLE_SIZE_KM_KEY = "triangle_size_km"
+    TRIANGLE_COLOR_KEY = "triangle_color"
 
     ## Defaults
     PLANE_PATH_WIDTH_KM = CONFIG_OPTIONS.get(PLANE_PATH_WIDTH_KM_KEY, 0.1)
@@ -123,6 +125,8 @@ class Plotter:
     SHORT_PARACHUTE_PATH_COLOR = CONFIG_OPTIONS.get(SHORT_PARACHUTE_PATH_COLOR_KEY, "red")
     LONG_PARACHUTE_PATH_WIDTH_KM = CONFIG_OPTIONS.get(LONG_PARACHUTE_PATH_WIDTH_KM_KEY, 3)
     LONG_PARACHUTE_PATH_COLOR = CONFIG_OPTIONS.get(LONG_PARACHUTE_PATH_COLOR_KEY, "orange")
+    TRIANGLE_SIZE_KM = CONFIG_OPTIONS.get(TRIANGLE_SIZE_KM_KEY, 0.2)
+    TRIANGLE_COLOR = CONFIG_OPTIONS.get(TRIANGLE_COLOR_KEY, "white")
 
 
     def __init__(self, **kwargs):
@@ -135,11 +139,71 @@ class Plotter:
         self.short_parachute_path_color = kwargs.get(self.SHORT_PARACHUTE_PATH_COLOR_KEY, self.SHORT_PARACHUTE_PATH_COLOR)
         self.long_parachute_path_width_km = float(kwargs.get(self.LONG_PARACHUTE_PATH_WIDTH_KM_KEY, self.LONG_PARACHUTE_PATH_WIDTH_KM))
         self.long_parachute_path_color = kwargs.get(self.LONG_PARACHUTE_PATH_COLOR_KEY, self.LONG_PARACHUTE_PATH_COLOR)
+        self.triangle_size_km = kwargs.get(self.TRIANGLE_SIZE_KM_KEY, self.TRIANGLE_SIZE_KM)
+        self.triangle_color = kwargs.get(self.TRIANGLE_COLOR_KEY, self.TRIANGLE_COLOR)
+
+
+    def _rotate_coordinate(self, x, y, angle):
+        """
+        Rotate a given coordinate around the origin by the specified angle.
+        """
+
+        sin = math.sin(angle)
+        cos = math.cos(angle)
+
+        x_ = x * cos - y * sin
+        y_ = x * sin + y * cos
+
+        return (x_, y_)
+
+
+    def _scale_coordinate(self, x, y, scale_factor):
+        """
+        Scale a given point by the given scale factor from the origin.
+        """
+
+        return (x * scale_factor, y * scale_factor)
+
+    
+    def _translate_coordinate(self, x1, y1, x2, y2):
+        """
+        Translate a given point relative another point.
+        """
+
+        return (x1 + x2, y1 + y2)
 
 
     def _plot_line(self, image, x1, y1, x2, y2, width, color):
+        """
+        Plot a line from (x1,y1) to (x2,y2) with a thickness of 'width' and colored in with 'color'.
+        """
+
         draw = ImageDraw.Draw(image, "RGBA")
         draw.line([x1, y1, x2, y2], fill=color, width=width)
+        del draw
+
+        return image
+
+
+    def _plot_triangle(self, image, x, y, rotation, side_length, color):
+        """
+        Plot a triangle centered on (x,y), rotated 'rotation' degrees clockwise from the east, and has side lengths of 
+        'side_length' and colored in with 'color'.
+        """
+
+        ## Precalculate the 'unit' triangle's centered coords, facing east
+        coords = [(-0.283, -0.5), (-0.283, 0.5), (0.567, 0)]
+        ## Calculate the triangle's side length scaling factor
+        scale_factor = side_length / 1
+
+        ## Scale, rotate, and translate the coords about the origin
+        for index, coord in enumerate(coords):
+            _x, _y = self._rotate_coordinate(*coord, -rotation)
+            _x, _y = self._scale_coordinate(_x, _y, scale_factor)
+            coords[index] = self._translate_coordinate(_x, _y, x, y)
+
+        draw = ImageDraw.Draw(image, "RGBA")
+        draw.polygon(coords, fill=color, outline=color)
         del draw
 
         return image
@@ -165,8 +229,9 @@ class Plotter:
         x2 = x + map_diagonal_length * math.cos(angle)
         y2 = y - map_diagonal_length * math.sin(angle)
 
-        ## Prep line widths
+        ## Prep widths
         plane_path_width = int(self.plane_path_width_km * pixels_per_km)
+        triangle_size = int(self.triangle_size_km * pixels_per_km)
         ## *2 because the width is only half of what it should be, since players can drop in any direction
         short_parachute_path_width = int(self.short_parachute_path_width_km * pixels_per_km * 2)
         long_parachute_path_width = int(self.long_parachute_path_width_km * pixels_per_km * 2)
@@ -176,6 +241,7 @@ class Plotter:
         plotted_map = self._plot_line(base_map, x1, y1, x2, y2, long_parachute_path_width, self.long_parachute_path_color)
         plotted_map = self._plot_line(base_map, x1, y1, x2, y2, short_parachute_path_width, self.short_parachute_path_color)
         plotted_map = self._plot_line(base_map, x1, y1, x2, y2, plane_path_width, self.plane_path_color)
+        plotted_map = self._plot_triangle(base_map, x, y, angle, triangle_size, self.triangle_color)
 
         ## Save and return the final map
         return self.file_controller.save_map(plotted_map)
